1f98def6ff3db918493f9c80f5dbbe1bfc8674b2
Lucy Wall <lhwall@gmail.com>
2018-08-16 00:31:50 +0000

    set up model ActiveRecord relationships

--- event-scheduler/app/models/event.rb
+++ event-scheduler/app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ActiveRecord::Base
+  belongs_to :category
+  belongs_to :user
 
   def self.upcoming_events
     #shows user all events that have not yet occured in chronological order
--- event-scheduler/app/models/user.rb
+++ event-scheduler/app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :events
+  has_many :categories, through: :events
 has_secure_password
 end
