682282d66b72561da500cd4fa03109839e974589
Lucy Wall <lhwall@gmail.com>
2018-08-16 00:52:35 +0000

    add actions to Event controller

--- event-scheduler/app/controllers/events_controller.rb
+++ event-scheduler/app/controllers/events_controller.rb
@@ -1,2 +1,25 @@
 class EventsController < ApplicationController
+
+  def index
+    @events = Event.all 
+  end 
+
+  def new
+    @event = Event.new 
+  end 
+
+  def create 
+  end
+
+  def show 
+    @event = Event.find(params[:id])
+  end 
+
+  def edit 
+    @event = Event.find(params[:id])
+  end
+
+  def update 
+  end 
+
 end
--- event-scheduler/app/models/category.rb
+++ event-scheduler/app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ActiveRecord::Base
+  has_many :events
+  has_many :users, through: :events
 end
