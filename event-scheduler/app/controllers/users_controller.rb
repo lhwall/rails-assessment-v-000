d1b0b09edc97e8bbd6225fe34f6a1914a2e8b4d6
Lucy Wall <lhwall@gmail.com>
2018-08-16 00:54:51 +0000

    add actions to Users controller

---
+++ event-scheduler/app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+
+  class EventsController < ApplicationController
+
+    def index
+      @users = User.all 
+    end 
+
+    def new
+      @user = User.new 
+    end 
+
+    def create 
+    end
+
+    def show 
+      @user = User.find(params[:id])
+    end 
+
+    def edit 
+      @user = User.find(params[:id])
+    end
+
+    def update 
+    end 
+
+  end
