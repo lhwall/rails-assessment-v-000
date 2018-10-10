089c9a16a8267f286f496109cc4678e9c8f8e7bc
Lucy Wall <lhwall@gmail.com>
2018-10-10 13:25:33 +0000

    move application methods to application helper

--- event-scheduler/app/controllers/sessions_controller.rb
+++ event-scheduler/app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
 
 def index
+  @events = Event.all
 end
 
-end 
+end
--- event-scheduler/app/helpers/application_helper.rb
+++ event-scheduler/app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  def current_user
+    if logged_in?
+      User.find(session[:user_id])
+    end
+  end
+
 end
