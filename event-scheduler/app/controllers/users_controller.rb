9b51a6d6c1aac4039a497941e27043d313354669
Lucy Wall <lhwall@gmail.com>
2018-10-15 19:16:48 +0000

    set up controller for new user

--- event-scheduler/app/controllers/users_controller.rb
+++ event-scheduler/app/controllers/users_controller.rb
@@ -1,7 +1,5 @@
 class UsersController < ApplicationController
 
-  class EventsController < ApplicationController
-
     def index
       @users = User.all
     end
@@ -11,6 +9,17 @@ class UsersController < ApplicationController
     end
 
     def create
+      if params[:username] == "" || params[:password] == ""
+            flash[:message] = "Please enter a username and password"
+            redirect to "/sign_up"
+          elsif !!User.find_by(username: params[:username])
+            flash[:message] = "That user name is taken"
+            redirect to "/sign_up"
+          else
+            @user = User.create(:username => params[:username], :password => params[:password])
+            session[:user_id] = @user.id
+            erb :"users/user_index"
+        end
     end
 
     def show
@@ -24,6 +33,11 @@ class UsersController < ApplicationController
     def update
     end
 
-  end
+
+private
+
+def user_params(*args)
+  params.require(:user).permit(*args)
+end
 
 end
