f795eabc8d279a98496549de38a832acc9d42781
Lucy Wall <lhwall@gmail.com>
2018-10-16 19:22:41 +0000

    debug user signup page

--- event-scheduler/app/controllers/users_controller.rb
+++ event-scheduler/app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
 
+
     def index
       @users = User.all
     end
 
     def new
       @user = User.new
+      render :layout => "loginsignup"
     end
 
     def create
