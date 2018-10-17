24016d09ed6e5aa8a59b0ad832de4a2fbaa46230
Lucy Wall <lhwall@gmail.com>
2018-10-17 23:50:35 +0000

    add login and logout routes

--- event-scheduler/app/controllers/sessions_controller.rb
+++ event-scheduler/app/controllers/sessions_controller.rb
@@ -4,4 +4,11 @@ def index
   @events = Event.all
 end
 
+def login
+end
+
+def logout
+  session[:user_id].clear
+end
+
 end
--- event-scheduler/config/routes.rb
+++ event-scheduler/config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :users
 
   root "sessions#index"
+
+  post "logout" => "sessions#logout"
+  post "login" => "sessions#login"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
