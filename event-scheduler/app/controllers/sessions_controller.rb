ce9a2724279ce9a0e28bdaf0ee7ac213748e4dc2
Lucy Wall <lhwall@gmail.com>
2018-10-18 15:26:39 +0000

    update login page

--- event-scheduler/app/controllers/sessions_controller.rb
+++ event-scheduler/app/controllers/sessions_controller.rb
@@ -6,9 +6,18 @@ end
 
 def login
   @user = User.new
+  render layout: false
 end
 
 def login_user
+  @user = User.find_by(:username => params[:username])
+ if @user && @user.authenticate(params[:password])
+   session[:user_id] = @user.id
+ redirect_to user_path(@user)
+ else
+   flash[:message] = "That is not the correct password or that username does not exist"
+   redirect_to login_path
+end
 end
 
 def logout
