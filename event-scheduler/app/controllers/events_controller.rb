905c3acf9a12027e28f8aa42dedb77a5f2a0454a
Lucy Wall <lhwall@gmail.com>
2018-10-30 15:05:06 +0000

    can only update an event with name and location

--- event-scheduler/app/controllers/events_controller.rb
+++ event-scheduler/app/controllers/events_controller.rb
@@ -38,9 +38,14 @@ end
     @event = Event.find(params[:id])
     #byebug
     if current_user == @event.user
+      if params[:event][:name] == "" || params[:event][:location] == "" || params[:event][:date] == ""|| params[:event][:time] == ""
+        flash[:message] = "Please include a name, location, date, and time for your event"
+        redirect_to edit_event_path(@event)
+  else
     @event.update(event_params(:name, :location, :date, :time, :description))
       flash[:message] = "Your event has been updated"
     redirect_to event_path(@event)
+    end
   else
     flash[:message] = "You do not have permission to edit this event"
     redirect_to event_path(@event)
