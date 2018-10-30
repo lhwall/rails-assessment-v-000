623e813ed18234eb321cb208f88b2f4408c5021e
Lucy Wall <lhwall@gmail.com>
2018-10-30 13:33:25 +0000

    add flash messages to event editing

--- event-scheduler/app/controllers/events_controller.rb
+++ event-scheduler/app/controllers/events_controller.rb
@@ -24,13 +24,20 @@ use Rack::Flash
 
   def edit
     @event = Event.find(params[:id])
+
   end
 
   def update
     @event = Event.find(params[:id])
+    if current_user.id == @event.user
     @event.update(event_params(:name, :location, :date, :time, :description))
+      flash[:message] = "Your event has been updated"
+    redirect_to event_path(@event)
+  else
+    flash[:message] = "You do not have permission to edit this event"
     redirect_to event_path(@event)
   end
+end
 
 private
 def event_params(*args)
