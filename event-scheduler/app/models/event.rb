7f714472e7e35829db9bd97d6d46070fa6d990f2
Lucy Wall <lhwall@gmail.com>
2018-10-30 19:30:43 +0000

    update scope method to ActiveRecord queries

--- event-scheduler/app/models/event.rb
+++ event-scheduler/app/models/event.rb
@@ -4,14 +4,7 @@ class Event < ActiveRecord::Base
 
   def self.upcoming_events
     #shows user all events that have not yet occured in chronological order
-    upcoming_events = []
-    Events.all do |event|
-      if Time.now < event.time
-        upcoming_events << event
-      end
-    end
-    upcoming_events.sort_by! {|a,b| a.date <=> b.date}
-    upcoming_events
+  Event.where(:date > Time.now).order(:date)
   end
 
 
