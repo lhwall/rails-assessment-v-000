707abb80f93784d974c7242a687051b7315971fe
Lucy Wall <lhwall@gmail.com>
2018-10-30 16:52:54 +0000

    set up OmniAuth for Facebook

--- event-scheduler/app/controllers/sessions_controller.rb
+++ event-scheduler/app/controllers/sessions_controller.rb
@@ -9,6 +9,17 @@ def login
   render layout: false
 end
 
+def create
+    @user = User.find_or_create_by(uid: auth['uid']) do |u|
+      u.name = auth['info']['name']
+      u.email = auth['info']['email']
+      u.image = auth['info']['image']
+    end
+
+    session[:user_id] = @user.id
+  redirect_to user_path(@user)
+  end
+
 def login_user
   @user = User.find_by(:username => params[:username])
   #byebug
@@ -28,5 +39,11 @@ def logout
   session.delete(:user_id)
   redirect_to events_path
 end
+  private
+
+  def auth
+    request.env['omniauth.auth']
+  end
+
 
 end
