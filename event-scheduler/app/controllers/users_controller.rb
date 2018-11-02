5b9fc4417c34a3543b1401a5dce710a96ffb4580
Lucy Wall <lhwall@gmail.com>
2018-11-02 17:43:52 +0000

    add helper methods for event dates and times

--- event-scheduler/app/controllers/users_controller.rb
+++ event-scheduler/app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
       @user = User.new(user_params)
     if @user.valid?
       @user.save
+      session[:user_id] = @user.id
      redirect_to user_path(@user)
     else
     @errors = @user.errors
--- event-scheduler/app/helpers/events_helper.rb
+++ event-scheduler/app/helpers/events_helper.rb
@@ -1,2 +1,11 @@
 module EventsHelper
+
+  def event_time(event)
+     event.time.strftime("%H:%M")
+  end
+
+  def event_date(event)
+    event.date.strftime("%A, %B %e %Y")
+  end
+
 end
